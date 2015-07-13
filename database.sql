0413575b0df4227e8822a9ffb9ae1010018094cc
slicvic <slicvic@Vics-MacBook-Pro.local>
2015-07-13 10:48:00 -0400

    Updated company and user profiles

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jul 13, 2015 at 04:37 PM
+-- Generation Time: Jul 13, 2015 at 04:47 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.5.10
 
@@ -13,6 +13,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `ssg`
 --
+CREATE DATABASE IF NOT EXISTS `ssg` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `ssg`;
 
 -- --------------------------------------------------------
 
