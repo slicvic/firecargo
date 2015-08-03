a93e5c8b2f46d724c4e6e0d0f25eeab7d0e9307d
slicvic <slicvic@Vics-MacBook-Pro.local>
2015-08-03 11:34:30 -0400

    Fix flash message

--- resources/views/flash_messages/classic/error.blade.php
+++ resources/views/flash_messages/classic/error.blade.php
@@ -1,4 +1,4 @@
 <div class="alert alert-danger">
-    <h4><i class="fa fa-times-circle"></i> {{ trans('messages.flash_error_title') }}:</h4>
+    <h4><i class="fa fa-times-circle"></i> {{ trans('messages.flash_error_title') }}</h4>
     {!! $message !!}
 </div>
